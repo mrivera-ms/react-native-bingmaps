b1b4f6c75b530685ed35fd1f7929ca19de6846d1
Monica Rivera <moniriv@microsoft.com>
2022-05-03 14:37:57 -0700

    Add Bing Maps sample

--- App.tsx
+++ App.tsx
@@ -1,12 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import * as React from 'react';
 
-const App = () => {
+import { StyleSheet, View, Text } from 'react-native';
+import BingMapsView from 'react-native-bing-maps';
+
+export default function App() {
   return (
-    <View>
-      <Text>Hello PhysOps</Text>
+    <View style={styles.container}>
+      <Text style={styles.mainText}>Hello PhysOps!</Text>
+      <BingMapsView
+        credentialsKey=""
+        mapLocation={{ lat: 12.9010875, long: 77.6095084, zoom: 15 }}
+        style={styles.box}
+      />
     </View>
-  )
+  );
 }
 
-export default App
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  box: {
+    height: 400,
+    width: 390,
+    marginVertical: 5,
+  },
+  mainText:{
+    fontSize: 30
+  }
+
+});
